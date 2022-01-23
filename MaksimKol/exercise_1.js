903f38800d4ba8cbf63b56f52244e18bb55461eb
DaZZeN_2 <qwezmaksim21@gmail.com>
2022-01-24 01:15:48 +0300

    fixed

--- MaksimKol/exercise_1.js
+++ MaksimKol/exercise_1.js
@@ -2,21 +2,21 @@ let a = 1;
 let b = -8;
 let c = 2;
 let D = b * b - 4 * a * c;
-let Number1;
-let Number2;
+let number1;
+let number2;
 
 if (D > 0) {
-    Number1 = (-b + Math.sqrt(D)) / (2 * a);
-    Number2 = (-b - Math.sqrt(D)) / (2 * a);
+    number1 = (-b + Math.sqrt(D)) / (2 * a);
+    number2 = (-b - Math.sqrt(D)) / (2 * a);
 
 console.log (`Дискриминант равен: ${D}`);
-console.log (`Корни уравнения: ${Number2}, ${Number2}`);
+console.log (`Корни уравнения: ${number2}, ${number2}`);
 
 } else if (D === 0) {
-    Number2 = Number2 = -b / (2 * a);
+    number2 = number2 = -b / (2 * a);
 
 console.log (`Дискриминант равен: ${D}`);
-console.log (`Корень уравнения повторяется и равен: ${(Number2 + Number2) / 2}`);
+console.log (`Корень уравнения повторяется и равен: ${(number2 + number2) / 2}`);
 
 } else {
 console.log ('В уравнении нет корней');
