b281be346ec7fd3e235fa7f6f9edeafbbc38a437
HellEaglee <helleagleee@gmail.com>
2022-01-23 19:55:24 +0300

    True student

--- Andrey/2/First/script.js
+++ Andrey/2/First/script.js
@@ -2,21 +2,21 @@
 
 const ratio = 4;
 
-let a = 10;
-let b = 55;
-let c = 33;
+let a = 1;
+let b = 3;
+let c = 2;
 
 let disc = b **2 - (ratio * a * c);
 
 
 
 if (disc === 0) {
-    console.log('Корень один единственный:' + ' ' + `${x}`);
     let x = (-b / (2 * a));
+    console.log('Корень один единственный:' + ' ' + `${x}`);
 } else if (disc < 0) {
     console.log('Нет корней');
 } else {
-    let x1 = (-b + Math.sqrt(disc) / (2 * a));
-    let x2 = (-b - Math.sqrt(disc) / (2 * a));
+    let x1 = ((-b + Math.sqrt(disc)) / (2 * a));
+    let x2 = ((-b - Math.sqrt(disc)) / (2 * a));
     console.log('Корни уравнения равны:' + ' ' + 'x1=' +  `${x1}` + ',' + ' ' + 'x2=' + `${x2}`);
 }
