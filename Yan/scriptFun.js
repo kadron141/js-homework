583fa435a92b5b58c39c318eb376568eab22d9f4
Yan Rzhevutski <kadron141@gmail.com>
2022-02-16 18:42:23 +0100

    finish todos

--- Yan/scriptFun.js
+++ Yan/scriptFun.js
@@ -27,6 +27,7 @@ const TODOS = [
     },
 ]
 
+let uniqueId = TODOS.length;
 
 
 const bodyTag = document.getElementById('root');
@@ -36,9 +37,25 @@ const header = document.createElement('header');
 const buttonDelete = document.createElement('button');
 const buttonAdd = document.createElement('button');
 const inputTodo = document.createElement('input');
+const todoContainer = document.createElement('div');
 inputTodo.placeholder = 'Enter todo...';
+inputTodo.id = 'main-input';
 buttonDelete.append(document.createTextNode('Delete All'));
 buttonAdd.append(document.createTextNode('Add'));
+
+
+buttonAdd.onclick = (_) => {
+    const input = document.getElementById('main-input');
+    if (input.value.length) {
+        const todoWrapper = getTodo({ id: uniqueId, text: input.value, date: (new Date()).toISOString().substring(0, 10) });
+        uniqueId++;
+        todoContainer.prepend(todoWrapper);
+    }
+}
+
+buttonDelete.onclick = (_) => {
+    todoContainer.innerHTML = '';
+}
 
 wrapper.className = 'main-wrapper';
 header.className = 'header-wrapper';
@@ -47,8 +64,9 @@ header.append(buttonDelete);
 header.append(inputTodo);
 header.append(buttonAdd);
 wrapper.append(header);
+wrapper.append(todoContainer);
 
-TODOS.forEach(todo => {
+const getTodo = (todo) => {
     const todoWrapper = document.createElement('div');
     todoWrapper.id = `todo_${todo.id}`;
     const todoBody = document.createElement('div');
@@ -57,6 +75,10 @@ TODOS.forEach(todo => {
     deleteTodo.append(document.createTextNode('X'));
     deleteTodo.id = `delete-todo_${todo.id}`;
 
+    const completeTodo = document.createElement('button');
+    completeTodo.append(document.createTextNode('Done!'));
+    completeTodo.id = `complete-todo_${todo.id}`;
+
     todoBody.append(document.createTextNode(todo.text));
     todoDate.append(document.createTextNode(todo.date));
 
@@ -64,18 +86,31 @@ TODOS.forEach(todo => {
     todoBody.className = 'todo-body';
     todoDate.className = 'todo-date';
 
+    todoWrapper.append(completeTodo);
     todoWrapper.append(todoBody);
     todoWrapper.append(todoDate);
     todoWrapper.append(deleteTodo);
-    wrapper.append(todoWrapper);
+
+    return todoWrapper;
+}
+
+TODOS.forEach(todo => {
+    const todoWrapper = getTodo(todo);
+    todoContainer.append(todoWrapper);
 })
 
-wrapper.addEventListener('click', (event) => {
-    if (event.target.nodeName === 'BUTTON') {
-        const id = event.target.id.split('-')[1];
+todoContainer.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'BUTTON') {
+        const id = e.target.id.split('-')[1];
         const todo = document.getElementById(id);
-        todo.parentNode.removeChild(todo);
-        console.log(todo);
+
+        if (e.target.id.split('-')[0] === 'delete') {
+            todo.parentNode.removeChild(todo);
+        } else {
+            todo.className += ' done';
+            const completeButton = document.getElementById(e.target.id);
+            completeButton.className += 'disabled';
+        }
     }
 })
 
